48b3fea20e422ecd9784ed06e0f2b26358bf42a2
Sarthak Anand <sarthakanand@MacBook-Pro.local>
2020-12-03 19:50:07 +0530

    bundles

---
+++ README.md
@@ -0,0 +1,1 @@
+## Dummy PyPi APP
--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(name='dummy-sarthak',
       version='0.0.0',
       description='First library',
-      author_email='isarth@me.com'
+      author_email='sarthak.anand@h2o.ai',
+      url='https://github.com/isarth23/dummy',
 )
